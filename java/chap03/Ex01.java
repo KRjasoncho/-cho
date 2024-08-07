4309bf00d2fd2161ca3ebc40a4e0bd37f38c92ad
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-08-07 20:55:40 +0900

    자바 스프링 연습 시작

---
+++ java/chap03/Ex01.java
@@ -0,0 +1,9 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        // 참/거짓 - boolean
+        System.out.println(true);
+        System.out.println(false);
+
+        //
+    }
+}
